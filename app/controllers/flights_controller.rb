80d4dd33784f896b3c2f70f38244e6f963f7594f
shrey-Odinproject <sonishreyf1@gmail.com>
2022-07-07 21:12:01 +0530

    Pull the flights which match that criteria from your database by detecting additional query parameters and querying model methods

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,5 +3,7 @@ class FlightsController < ApplicationController
     @date_options = Flight.all.map { |f| [f.start, f.start] }
     @num_passenger_options = *(1..4).map { |n| [n, n] }
     @all_airport_options = Airport.all.map { |a| [a.code, a.id] }
+    @result = Flight.find_by(departure_airport_id: params['departure_airport_id'],
+      arrival_airport_id: params['arrival_airport_id'], start: params['start'])
   end
 end
