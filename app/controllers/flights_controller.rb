f04690b58df7cf80e8ec821742e8c081501de34e
shrey-Odinproject <sonishreyf1@gmail.com>
2022-07-07 20:48:01 +0530

    Add the four dropdown menus - a list of departure airports, arrival airports, number of passengers 1-4, and a date dropdown for the flight date
    The date dropdown only includes dates that have existing flights,
    allow the user to select any airport not just, no constraint to show
    only the airports that have arrival,departure.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,7 @@
 class FlightsController < ApplicationController
   def index
-    
+    @date_options = Flight.all.map { |f| [f.start, f.start] }
+    @num_passenger_options = *(1..4).map { |n| [n, n] }
+    @all_airport_options = Airport.all.map { |a| [a.code, a.id] }
   end
 end
