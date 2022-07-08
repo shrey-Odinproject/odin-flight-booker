303c7a0c10efd414212a804bdc773f0d8f415aa6
shrey-Odinproject <sonishreyf1@gmail.com>
2022-07-08 17:07:22 +0530

    Display search results in their own form with radio button to select single result.
    The user will select which flight to submit and move onto the next step.
    The form should submit to the #new action of the BookingsController (not
    yet created) using GET.
    Result form also includes a hidden field that contains the number of passengers.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,9 +1,12 @@
 class FlightsController < ApplicationController
   def index
-    @date_options = Flight.all.map { |f| [f.start, f.start] }
+    @date_options = Flight.distinct.pluck(:start, :start) # as we want array of arrays and pluck directly gives an array 
     @num_passenger_options = *(1..4).map { |n| [n, n] }
-    @all_airport_options = Airport.all.map { |a| [a.code, a.id] }
+    @all_airport_options = Airport.all.map { |a| [a.code, a.id] } # can replace with  Airport.pluck(:code, :id)
+
     @result = Flight.where(departure_airport_id: params['departure_airport_id'],
-      arrival_airport_id: params['arrival_airport_id'], start: params['start'])
+              arrival_airport_id: params['arrival_airport_id'], start: params['start'])
+
+    @passenger_result = params[:passenger]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,21 @@
 Airport.destroy_all
 Flight.destroy_all
 
-a1=Airport.create(code:'NYC')
-a2=Airport.create(code:'MUM')
-a3=Airport.create(code:'LON')
-a4=Airport.create(code:'SYD')
-a5=Airport.create(code:'PAR')
+a1 = Airport.create(code: 'NYC')
+a2 = Airport.create(code: 'MUM')
+a3 = Airport.create(code: 'LON')
+a4 = Airport.create(code: 'SYD')
+a5 = Airport.create(code: 'PAR')
 
+airports = [a1, a2, a3, a4, a5]
+dates = [DateTime.parse("14/12/2022 08:00"), DateTime.parse("22/11/2022 20:00"), DateTime.parse("10/07/2022 18:30"),
+         DateTime.parse("17/04/2022 09:00"), DateTime.parse("03/03/2022 16:15")]
 p "created #{Airport.count} airports"
 
-Flight.create(start: DateTime.parse("14/12/2022 08:00"), duration: 90, departure_airport_id: a2.id, arrival_airport_id: a5.id)
-Flight.create(start: DateTime.parse("22/11/2022 20:00"), duration: 360, departure_airport_id: a1.id, arrival_airport_id: a3.id)
-Flight.create(start: DateTime.parse("10/07/2022 18:30"), duration: 60, departure_airport_id: a4.id, arrival_airport_id: a2.id)
-Flight.create(start: DateTime.parse("17/04/2022 09:00"), duration: 120, departure_airport_id: a3.id, arrival_airport_id: a5.id)
-Flight.create(start: DateTime.parse("03/03/2022 16:15"), duration: 270, departure_airport_id: a5.id, arrival_airport_id: a4.id)
-Flight.create(start: DateTime.parse("18/09/2022 13:20"), duration: 600, departure_airport_id: a2.id, arrival_airport_id: a1.id)
+20.times do
+  port1, port2 = airports.sample(2)
+  Flight.create(start: dates.sample,
+                duration: rand(1..10), departure_airport_id: port1.id, arrival_airport_id: port2.id)
+end
 
 p "created #{Flight.count} flights"
