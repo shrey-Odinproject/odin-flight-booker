3a9e3b33917c79c295e67ecfa4cbaf6a49304074
shrey-Odinproject <sonishreyf1@gmail.com>
2022-07-13 23:02:55 +0530

    Make create and show method for booking using strong params (nested) which creates booking related passengers as well
    Display all details at bookings#show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,5 @@
 class BookingsController < ApplicationController
+  
   def new
     @flight = Flight.find(params['flight_id'])
     @no_of_passengers = params['passenger'].to_i
@@ -6,7 +7,26 @@ class BookingsController < ApplicationController
     @booking = Booking.new
     @no_of_passengers.times { @booking.passengers.build }
   end
+  
   def create
+    @booking = Booking.new(booking_params)
 
+    if @booking.save
+      flash[:success] = "Great! Your booking has been created!"
+      redirect_to @booking
+    else
+      flash.now[:error] = 'Booking failed'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
   end
 end
